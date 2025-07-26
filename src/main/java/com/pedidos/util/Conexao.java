f577e0d84ae70a4ecc20a68e4b09016630878572
Ana_Carla_Guimarães <annacarlabarbosaguimaraes49@gmail.com>
2025-07-26 09:15:34 -0300

    Arquivo pom.xml e conexão ao banco de dados

--- src/main/java/com/pedidos/util/Conexao.java
+++ src/main/java/com/pedidos/util/Conexao.java
@@ -1,5 +1,15 @@
 package main.java.com.pedidos.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
-    
+    private static final String URL = "jdbc:postgresql://localhost:5432/gerenciamento_pedidos";
+    private static final String USUARIO = "postgres";
+    private static final String SENHA = "AnaCarlaBG"; 
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL, USUARIO, SENHA);
+    }
 }
